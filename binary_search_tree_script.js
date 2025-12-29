155e6ceb7a6feb8e1a9486c8dc1d376966e7fbff
Falesz <jnjwrg@inf.elte.hu>
2025-12-29 22:47:41 +0100

    Add numbers to the static graph drawing

--- binary_search_tree_script.js
+++ binary_search_tree_script.js
@@ -4,26 +4,27 @@ const treeCanvasContext = treeCanvas.getContext("2d");
 treeCanvasContext.strokeStyle = "black";
 treeCanvasContext.lineWidth = 5;
 
+treeCanvasContext.fillStyle = "black";
+treeCanvasContext.font = "42px Arial";
+treeCanvasContext.textAlign = "center";
+treeCanvasContext.textBaseline = "middle";
+
 
 treeCanvasContext.beginPath();
-
 treeCanvasContext.arc(300, 300, 50, 0, Math.PI * 2);
-
 treeCanvasContext.stroke();
+treeCanvasContext.fillText("3", 300, 300);
 
 
 
 treeCanvasContext.beginPath();
-
 treeCanvasContext.arc(150, 450, 50, 0, Math.PI * 2);
-
 treeCanvasContext.stroke();
+treeCanvasContext.fillText("5", 150, 450);
 
 
 
 treeCanvasContext.beginPath();
-
 treeCanvasContext.moveTo(250, 350);
-
 treeCanvasContext.lineTo(200, 400);
 treeCanvasContext.stroke();
